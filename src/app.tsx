3106719d610ff99024031a916afeba1df70f259e
neulhi <neu1hi.work@gmail.com>
2025-01-20 12:00:16 +0900

    [실습] Chip & ChipList 컴포넌트 작성 I

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,7 @@
+import Playground from './playground';
+
 function App() {
-  return (
-    <div className="prose">
-      <header>
-        <h1 lang="en">Custom Vite Template</h1>
-      </header>
-      <main>
-        <p>React 프로젝트 시작을 위한 사용자 정의 템플릿</p>
-      </main>
-      <footer>
-        <small lang="en">
-          &copy; {new Date().getFullYear()} EUID. All rights reserved.
-        </small>
-      </footer>
-    </div>
-  );
+  return <Playground />;
 }
 
 export default App;
