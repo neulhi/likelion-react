9000235c0eb35d06924459b823041311ec22ea5a
neulhi <neu1hi.work@gmail.com>
2025-01-21 10:53:18 +0900

    [실습] 회원가입 처리 II

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt';
 import express from 'express';
 import { createUser } from './lib/user.js';
 
--- backend/lib/user.js
+++ backend/lib/user.js
@@ -1,1 +1,16 @@
-export async function createUser () {} 
+import { readFile } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+
+// /Users/yamoo9/Desktop/likelion-react/backend/data/users.json
+const FILE_PATH = fileURLToPath(new URL('../data/users.json', import.meta.url));
+
+export async function getUsers() {
+  const users = await readFile(FILE_PATH, { encoding: 'utf-8' });
+  return JSON.parse(users);
+}
+
+export async function findUserByEmail() {}
+
+export async function isRegisteredUser() {}
+
+export async function createUser() {}
