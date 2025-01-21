0c8d3a79630280d85bacd8ae06df28e564471247
neulhi <neu1hi.work@gmail.com>
2025-01-21 11:06:57 +0900

    [실습] 회원가입 처리 III - findUserByEmail 함수

--- backend/lib/user.js
+++ backend/lib/user.js
@@ -3,13 +3,20 @@ import { fileURLToPath } from 'node:url';
 
 // /Users/yamoo9/Desktop/likelion-react/backend/data/users.json
 const FILE_PATH = fileURLToPath(new URL('../data/users.json', import.meta.url));
+const OPTIONS = { encoding: 'utf-8' };
 
 export async function getUsers() {
-  const users = await readFile(FILE_PATH, { encoding: 'utf-8' });
+  const users = await readFile(FILE_PATH, OPTIONS);
   return JSON.parse(users);
 }
 
-export async function findUserByEmail() {}
+export async function findUserByEmail(email) {
+  const users = await getUsers();
+  return users.find((user) => user.email === email);
+}
+
+const user = await findUserByEmail('hanj2@kakao.com');
+console.log(user);
 
 export async function isRegisteredUser() {}
 
