296bd5e9f7bbd4900e669b66b86af652ccdb9391
neulhi <neu1hi.work@gmail.com>
2025-02-03 15:56:14 +0900

    [실습] clsx 유틸리티 함수 활용 - clsx/lite 모듈 활용 - UI 축 변경 되지 않도록 설정 (overflow-y-scroll)

--- src/homework/components/nav/index.tsx
+++ src/homework/components/nav/index.tsx
@@ -1,6 +1,15 @@
 import { useState } from 'react';
+import clsx from 'clsx/lite';
 import { getUIView, type UIView } from '@/homework/lib/ui-view';
 import './style.css';
+
+// clsx/lite 모듈
+// console.log(clsx('a', 'b', props.data && 'c'));
+// console.log(clsx('a', 'b', false && 'd'));
+
+// clsx 모듈
+// console.log(clsx('a', 'b', { 'my-class-name': props.data }));
+// console.log(clsx('a', 'b', ['d']));
 
 function Nav() {
   const [uiView] = useState<UIView>(getUIView);
@@ -9,10 +18,20 @@ function Nav() {
   return (
     <nav className="nav">
       <h2 className="sr-only">페이지 탐색</h2>
-      <a href="/?view=signin" className={isSignInView ? 'active' : undefined}>
+      <a
+        href="/?view=signin"
+        // className={isSignInView ? 'active' : undefined}
+        className={clsx(isSignInView && 'active')}
+        aria-current={isSignInView ? 'page' : undefined}
+      >
         로그인
       </a>
-      <a href="/?view=signup" className={!isSignInView ? 'active' : undefined}>
+      <a
+        href="/?view=signup"
+        // className={!isSignInView ? 'active' : undefined}
+        className={clsx(!isSignInView && 'active')}
+        aria-current={!isSignInView ? 'page' : undefined}
+      >
         회원가입
       </a>
     </nav>
