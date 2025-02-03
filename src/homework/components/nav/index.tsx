c64c5b4ba3d34eb4c895f2a8435077a4d2a788f4
neulhi <neu1hi.work@gmail.com>
2025-02-03 17:41:15 +0900

    [실습] 페이지 내비게이션 구성 - switch 문 활용 - 파생된 상태 활용

--- src/homework/components/nav/index.tsx
+++ src/homework/components/nav/index.tsx
@@ -1,26 +1,22 @@
-import { useState } from 'react';
 import clsx from 'clsx/lite';
+import { useState } from 'react';
 import { getUIView, type UIView } from '@/homework/lib/ui-view';
 import './style.css';
 
-// clsx/lite 모듈
-// console.log(clsx('a', 'b', props.data && 'c'));
-// console.log(clsx('a', 'b', false && 'd'));
-
-// clsx 모듈
-// console.log(clsx('a', 'b', { 'my-class-name': props.data }));
-// console.log(clsx('a', 'b', ['d']));
-
 function Nav() {
+  // 상태
   const [uiView] = useState<UIView>(getUIView);
+
+  // 파생된 상태
   const isSignInView = uiView.includes('signin');
+  const isSignUpView = uiView.includes('signup');
+  const isStateManagementView = uiView.includes('state-management');
 
   return (
     <nav className="nav">
       <h2 className="sr-only">페이지 탐색</h2>
       <a
         href="/?view=signin"
-        // className={isSignInView ? 'active' : undefined}
         className={clsx(isSignInView && 'active')}
         aria-current={isSignInView ? 'page' : undefined}
       >
@@ -28,11 +24,17 @@ function Nav() {
       </a>
       <a
         href="/?view=signup"
-        // className={!isSignInView ? 'active' : undefined}
-        className={clsx(!isSignInView && 'active')}
-        aria-current={!isSignInView ? 'page' : undefined}
+        className={clsx(isSignUpView && 'active')}
+        aria-current={isSignUpView ? 'page' : undefined}
       >
         회원가입
+      </a>
+      <a
+        href="/?view=state-management"
+        className={clsx(isStateManagementView && 'active')}
+        aria-current={isStateManagementView ? 'page' : undefined}
+      >
+        상태 관리 전략
       </a>
     </nav>
   );
