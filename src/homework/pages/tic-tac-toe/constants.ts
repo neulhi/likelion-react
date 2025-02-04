9ddb7ea138522677ee37640d9a3dde87a7015be2
neulhi <neu1hi.work@gmail.com>
2025-02-04 15:40:46 +0900

    [실습] 틱택토 게임 구현 part 2. - Grid 컴포넌트 상태 - Grid 컴포넌트 파생된 상태 - Grid 컴포넌트 상태 업데이트 함수

---
+++ src/homework/pages/tic-tac-toe/constants.ts
@@ -0,0 +1,14 @@
+// ----------------------------------------------
+// 게임 상수
+// ----------------------------------------------
+
+/* const INITIAL_CELLS = [null, null, null, null, null, null, null, null, null]; */
+export const INITIAL_CELLS = Array(9).fill(null);
+
+export const enum PLAYER {
+  ONE = '🥵',
+  TWO = '🥶',
+}
+
+export type Player = null | PLAYER;
+export type Cells = (Player[] | null)[];
