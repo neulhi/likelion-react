99c30294b1152c22496dc449c0b42976187bb420
neulhi <neu1hi.work@gmail.com>
2025-02-06 11:34:10 +0900

    [실습] 게임 위너 - 게임 진행 중 위너 찾기 함수 작성 - 위너가 있을 경우, 말판에 위너 스타일링

--- src/homework/pages/tic-tac-toe/constants.ts
+++ src/homework/pages/tic-tac-toe/constants.ts
@@ -11,3 +11,39 @@ export const enum PLAYER {
 }
 
 export type Cells = (PLAYER | null)[];
+
+// 게임 승리 조건
+const WINNER_CONDITIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+type Winner = {
+  player: PLAYER;
+  condition: [number, number, number];
+} | null;
+
+export const getWinner = (cells: Cells) => {
+  let winner = null;
+
+  // 승리 조건 순환
+  for (const [x, y, z] of WINNER_CONDITIONS) {
+    const player = cells[x];
+    // 승리 조건에 해당하는 위치에 말판이 일치하는 지 검사
+    if (player && cells[y] === player && cells[z] === player) {
+      winner = {
+        player,
+        condition: [x, y, z],
+      };
+      break;
+    }
+  }
+
+  return winner as Winner;
+};
