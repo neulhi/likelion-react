162fbf7dd70b67f6654b0ade723fd9b5bddfd35f
neulhi <neu1hi.work@gmail.com>
2025-02-12 11:32:00 +0900

    [실습] useRef 훅 활용 (렌더링에 무관한 데이터 메모리)

--- src/pages/memory-component-data/components/counter.function.tsx
+++ src/pages/memory-component-data/components/counter.function.tsx
@@ -1,72 +1,71 @@
 import { tm } from '@/utils/tw-merge';
-import React from 'react';
+import { useRef, useState } from 'react';
 
-class CounterClass extends React.Component {
-  state = {
-    count: 9,
-  };
+function CounterFunction() {
+  // 리액트가 반응하는 데이터
+  const [count, setCount] = useState(9);
 
-  // 클래스 컴포넌트의 인스턴스는
-  // this를 사용해 자신의 속성을 기억
-  prevCount = this.state.count;
+  // 상태 업데이트 요청 (무한 루프 -> 컴퓨터 다운)
+  // setCount(count + 1);
 
-  render() {
-    // 현재 랜더링 시점의 상태 값(스냅샷: 불변)
-    const { count } = this.state;
-    // 현재 렌더링 시점의 상태 값을 기억
-    // render() 메서드 외부에 [인스턴스 멤버]를 사용해서
-    const { prevCount } = this;
+  // 렌더링과 무관한 데이터 값 기억
+  // 참조(ref) 객체의 현재(current) 값을 수정해도
+  // 리액트는 반응하지 않는다. (반응성 데이터가 아니기 때문)
+  const prevCountValueRef = useRef(count); // { current: 9 }
 
-    console.log('이전 count:', prevCount);
-    console.log('현재 count:', count);
+  // 현재 count 값을 prevCount 변수에 할당해도
+  // 다음 렌더링에 prevCount 변수는 초기화 됨
+  let prevCount = count;
 
-    // 지역 변수는 기억할 수 없다. (함수처럼 초기화 됨)
-    let componentType = 'class';
-    console.log(componentType);
+  // 결과적으로 prevCount, count는 항상 값이 동일
+  console.log('prevCountValueRef.current = ', prevCountValueRef.current);
+  console.log('prevCount = ', prevCount);
+  console.log('현재 count = ', count);
 
-    componentType = 'functional';
-    console.log(componentType);
+  const handleIncrease = () => {
+    // 다음 렌더링에서 이전 count 값 기억 (실패)
+    prevCount = count;
 
-    return (
-      <>
-        <button
-          type="button"
-          onClick={this.handleIncrease}
-          className={tm(
-            'cursor-pointer opacity-90',
-            'grid place-content-center',
-            'bg-react text-white px-7 py-0.5 rounded-full font-semibold',
-            'hover:opacity-100'
-          )}
-        >
-          {count}
-        </button>
-
-        <dl className="border-4 rounded-md p-4 my-4 w-48 flex flex-col gap-2">
-          <div className="flex justify-between">
-            <dt className="text-slate-900">이전 count 값</dt>
-            <dd className="font-black">{prevCount}</dd>
-          </div>
-          <div className="flex justify-between">
-            <dt className="text-slate-900">현재 count 값</dt>
-            <dd className="font-black">{count}</dd>
-          </div>
-        </dl>
-      </>
-    );
-  }
-
-  handleIncrease = () => {
-    const { count } = this.state;
-
-    // 다음 렌더링에서 이전 count 값 기억 (가능)
-    this.prevCount = count;
+    // 참조(ref) 객체의 현재(current) 값 수정 (기억 성공!)
+    prevCountValueRef.current = count;
 
     // 다음 렌더링에 사용될 상태 값 업데이트
-    this.setState({
-      count: count + 1,
-    });
+    setCount(count + 1);
   };
+
+  return (
+    <>
+      <button
+        type="button"
+        onClick={handleIncrease}
+        className={tm(
+          'cursor-pointer opacity-90',
+          'grid place-content-center',
+          'bg-react text-white px-7 py-0.5 rounded-full font-semibold',
+          'hover:opacity-100'
+        )}
+      >
+        {count}
+      </button>
+
+      <dl className="border-4 rounded-md p-4 my-4 w-102 flex flex-col gap-2">
+        <div className="flex justify-between">
+          <dt className="text-slate-900">이전 count 값 (prevCount)</dt>
+          <dd className="font-black">{prevCount}</dd>
+        </div>
+        <div className="flex justify-between">
+          <dt className="text-slate-900">
+            이전 count 값 (prevCountValueRef.current)
+          </dt>
+          <dd className="font-black">{prevCountValueRef.current}</dd>
+        </div>
+        <div className="flex justify-between">
+          <dt className="text-slate-900">현재 count 값</dt>
+          <dd className="font-black">{count}</dd>
+        </div>
+      </dl>
+    </>
+  );
 }
 
-export default CounterClass;
+export default CounterFunction;
