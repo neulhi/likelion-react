19612e97bdef216f854a4b1bda0ef7c93be28d15
neulhi <neu1hi.work@gmail.com>
2025-02-12 15:41:45 +0900

    [실습] DOM 접근/조작 1 - JSX는 HTML이 아니다. - JSX는 React API로 컴파일 - 컴파일 결과는 JavaScript - 실제 DOM에 접근하려면 ref 콜백 함수를 사용한다.

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -8,18 +8,32 @@ import TicTacToe from '@/pages/tic-tac-toe';
 import SignInForm from '@/pages/sign-in';
 import SignUpForm from '@/pages/sign-up';
 import NotFound from '@/pages/not-found';
+import AccessDOMPage from '@/pages/access-dom';
 
 // --------------------------------------------------------------------------
 // 내비게이션 리스트 데이터
 
 export const navList = [
-  { path: 'memory-component-data', text: '데이터 메모리', element: <MemoryComponentDataPage /> },
+  { path: 'accessing-dom', text: 'DOM 접근/조작', element: <AccessDOMPage /> },
+  {
+    path: 'memory-component-data',
+    text: '데이터 메모리',
+    element: <MemoryComponentDataPage />,
+  },
   { path: 'search-list', text: '검색 리스트', element: <SearchListPage /> },
   { path: 'side-effect', text: '이펙트 처리', element: <SideEffectDemo /> },
-  { path: 'error-boundary', text: '에러 바운더리', element: <ErrorBoundaryDemo />,},
+  {
+    path: 'error-boundary',
+    text: '에러 바운더리',
+    element: <ErrorBoundaryDemo />,
+  },
   { path: 'lifecycles', text: '라이프 사이클', element: <LifecycleDemo /> },
   { path: 'tic-tac-toe', text: '틱택토', element: <TicTacToe /> },
-  { path: 'state-management', text: '상태 관리 전략', element: <StateManagement />, },
+  {
+    path: 'state-management',
+    text: '상태 관리 전략',
+    element: <StateManagement />,
+  },
   { path: 'signup', text: '회원가입', element: <SignUpForm /> },
   { path: 'signin', text: '로그인', element: <SignInForm /> },
 ];
