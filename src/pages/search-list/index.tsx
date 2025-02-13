654acb66c2a35f8030985bb31634127be0071487
neulhi <neu1hi.work@gmail.com>
2025-02-13 14:45:59 +0900

    [실습] 다른 컴포넌트 DOM 접근/조작 3 - 명령형 핸들러(기능, 함수)를 상위 컴포넌트에 공유하는 방법 - useImperativeHandle 훅 활용

--- src/pages/search-list/index.tsx
+++ src/pages/search-list/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { tm } from '@/utils/tw-merge';
 import { getQueryParam } from './utils/query-param';
 import SearchedList from './components/searched-list';
@@ -17,33 +17,62 @@ function SearchListPage() {
     );
   };
 
-  // 지연된 초기화(lazy initializer)
-  // useState() 훅에 설정된 함수
   const [query, setQuery] = useState(getQueryState);
 
-  // 이펙트 처리
   useEffect(() => {
-    // popstate 이벤트 구독/해지
-    // 이벤트 핸들러 (동일 참조)
     const handlePopState = () => {
-      // 브라우저 popstate 이벤트가 감지될 때
-      // 리액트 앱의 query 상태 업데이트 -> UI 화면 업데이트
       setQuery(getQueryState);
     };
 
-    // 이벤트 구독
     globalThis.addEventListener('popstate', handlePopState);
 
-    // 이벤트 해지
     return () => {
       globalThis.removeEventListener('popstate', handlePopState);
     };
   }, []);
 
+  // --------------------------------------------------------------------------
+
+  // 참조 객체
+  const sharedImperativeHandlesRef = useRef<{
+    focus: () => void;
+    select: () => void;
+    remove: () => void;
+  }>(null);
+
+  useEffect(() => {
+    const clearId = setTimeout(() => {
+      const handles = sharedImperativeHandlesRef.current;
+      if (handles) {
+        handles.select();
+
+        // [문제 상황]
+        // 자식이 모든 권한을 부여했으므로
+        // 자식 내부에 있는 감춰진 요소에 무슨 짓을 해도 된다.
+        // 문제가 발생할 수 있다. 누군가에 의해....
+        // 그 코드를 디버깅하기가 쉽지 않다.
+
+        // [문제 해결]
+        // 이런 어처구니 없는 상황이 누군가에 의해 발생하지 않도록
+        // 컴포넌트 DOM을 전면 공개할 것이 아니라, 일부 명령형 기능(함수)만
+        // 상위 컴포넌트에 공유하겠노라!!!
+        // sharedImperativeHandlesRef.current.remove();
+      }
+    }, 900);
+
+    return () => {
+      clearTimeout(clearId);
+    };
+  });
+
   return (
     <section className={tm('flex flex-col gap-5 items-center')}>
       <h2 className="text-2xl font-medium text-react">카드 검색 리스트</h2>
-      <SearchForm query={query} setQuery={setQuery} />
+      <SearchForm
+        ref={sharedImperativeHandlesRef}
+        query={query}
+        setQuery={setQuery}
+      />
       <SearchedList list={list} query={query} onUpdate={handleUpdateList} />
     </section>
   );
