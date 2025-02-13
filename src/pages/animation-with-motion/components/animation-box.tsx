95f5b104ac8fd9a460acd4790af63026bdd20585
neulhi <neu1hi.work@gmail.com>
2025-02-13 10:37:36 +0900

    [실습] 애니메이션 스태거 - Map 데이터 활용 - getItemMap : useRef 사용 시, 중복 생성하지 않도록 설정 - 리스트 렌더링 ref 순환 참조 - 이펙트에서 시간차 애니메이션 설정

--- src/pages/animation-with-motion/components/animation-box.tsx
+++ src/pages/animation-with-motion/components/animation-box.tsx
@@ -10,15 +10,18 @@ function AnimationBox({
 
   useEffect(() => {
     const boxElement = boxRef.current;
+
     if (boxElement) {
       animate(
         // 애니메이션 대상
         boxElement,
         // 애니메이션 속성 (CSS 속성, JavaScript 객체 또는 배열 데이터, SVG, ...)
         {
-          // x: 240,
-          rotate: -360,
-          scale: 0.8,
+          // x: 240 /* 24px 만큼 이동 */,
+          rotate: -360 /* -360deg 만큼 회전 */,
+          scale: 0.55,
+          x: -28,
+          y: -32,
         },
         // 애니메이션 옵션
         {
@@ -37,7 +40,7 @@ function AnimationBox({
         'flex justify-center items-center',
         'size-30',
         'bg-react text-white text-lg font-medium',
-        'rounded-lg',
+        'rounded-xl',
         className
       )}
       {...restProps}
