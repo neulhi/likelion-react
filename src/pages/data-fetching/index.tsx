45603daec7ccd6a8ad4ef5a47382af23a46c7f7f
neulhi <neu1hi.work@gmail.com>
2025-02-17 11:53:58 +0900

    [실습] 데이터 추가 뮤테이션 - RecipeCreate

--- src/pages/data-fetching/index.tsx
+++ src/pages/data-fetching/index.tsx
@@ -1,10 +1,20 @@
+import RecipeCreate from './components/RecipeCreate';
+import RecipeDelete from './components/RecipeDelete';
+import RecipeEdit from './components/RecipeEdit';
 import RecipeList from './components/RecipeList';
 import RecipeSingle from './components/RecipeSingle';
 
 function DataFetchingPage() {
   return (
     <section className="flex flex-col gap-5 my-5">
-      <h2 className="text-2xl font-medium">데이터 가져오기</h2>
+      <h2 className="text-xl font-medium">데이터 변형(Data Mutations)</h2>
+      <RecipeCreate />
+      <RecipeEdit />
+      <RecipeDelete />
+
+      <hr />
+
+      <h2 className="text-2xl font-medium">데이터 쿼리(Data Query)</h2>
 
       <h3 className="text-xl font-medium">레시피 리스트</h3>
       <RecipeList />
