1c91740fd55480c000d7827c88c5bc408e76dd30
neulhi <neu1hi.work@gmail.com>
2025-02-21 10:19:14 +0900

    [실습] 셀렉터 함수를 사용하는 이유 - Getter / Setter 구분 - 성능 저하 방지 - 불필요한 리-렌더 차단

--- src/stores/count.ts
+++ src/stores/count.ts
@@ -1,38 +1,40 @@
 import { create } from 'zustand';
+import { combine } from 'zustand/middleware';
 
-interface State {
-  count: number;
-  step: number;
-  min: number;
-  max: number;
-}
-
-interface Actions {
-  count: number;
-  step: number;
-  min: number;
-  max: number;
-  increment: () => void;
-  decrement: () => void;
-  update: (value: number) => void;
-  reset: () => void;
-  setStep: (value: number) => void;
-  setMin: (value: number) => void;
-  setMax: (value: number) => void;
-}
-
-type Store = State & Actions;
-
-export const useCountStore = create<Store>((set) => ({
-  count: 0,
+// 초기 상태
+const initialState = {
+  count: 1,
   step: 1,
   min: 1,
-  max: 10,
-  increment: () => set((state) => ({ count: state.count + 1 })),
-  decrement: () => set((state) => ({ count: state.count - 1 })),
-  update: (value) => set({ count: value }),
-  setStep: (value) => set({ step: value }),
-  setMin: (value) => set({ min: value }),
-  setMax: (value) => set({ max: value }),
-  reset: () => set({ count: 0 }),
-}));
+  max: 100,
+};
+
+// 파생된 상태
+
+
+export const useCountStore = create(
+  // 병합(combine) 미들웨어
+  // 상태 + 스토어 생성 함수(액션만 포함)
+
+  // 상태가 변경되면 컴포넌트가 리-렌더링 된다. (!)
+  // 상태 업데이트 함수가 실행되면 컴포넌트가 리-렌더링 된다. (?)
+  combine(
+    { 
+      ...initialState 
+    }, 
+    (set) => ({
+      increment: () => set(({ count, step, max }) => {
+        const nextCount = count + step;
+        return {
+          count: nextCount > max ? max : nextCount,
+        }
+      }),
+      decrement: () => set(({ count, step, min }) => {
+        const nextCount = count - step;
+        return {
+          count: nextCount < min ? min : nextCount,
+        }
+      }),
+      reset: () => set(initialState),
+    }))
+);
